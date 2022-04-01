3e19cfd2d0ac9d1e26ee8496e7cda818826c6404
AdamMusa <adammusa2222@gmail.com>
2022-04-01 17:31:24 +0100

    import latlong2 package

--- lib/plugin_api.dart
+++ lib/plugin_api.dart
@@ -4,3 +4,4 @@ export 'package:flutter_map/flutter_map.dart';
 export 'package:flutter_map/src/core/bounds.dart';
 export 'package:flutter_map/src/core/center_zoom.dart';
 export 'package:flutter_map/src/map/map.dart';
+import 'package:latlong2/latlong.dart';
